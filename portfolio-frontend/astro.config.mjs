6de6c8614f61d25ca5cac5c8b7a205a56af2bbdd
nickmarcha <nicolasmmjoes@gmail.com>
2026-03-01 17:44:13 +0100

    frontend deployment fix

--- portfolio-frontend/astro.config.mjs
+++ portfolio-frontend/astro.config.mjs
@@ -2,11 +2,14 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://portfolio.nickmarcha.com',
 	base: '/',
 	integrations: [mdx(), sitemap()],
+	image: {
+		service: passthroughImageService(),
+	},
 });
