36bda7428b2bf5c809ae31e522ff3c898ac4870c
nickmarcha <nicolasmmjoes@gmail.com>
2026-03-01 17:22:32 +0100

    deployment fix

--- portfolio-frontend/astro.config.mjs
+++ portfolio-frontend/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nickmarcha.github.io',
-	base: '/portfolio/',
+	site: 'https://portfolio.nickmarcha.com',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
